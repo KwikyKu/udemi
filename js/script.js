25169632902def82898a2d1e6e784667208d7fad
Gulnara <kubaguschewa.gulnara@yandex.ru>
2023-09-27 20:13:42 +0600

    cycle1

--- js/script.js
+++ js/script.js
@@ -1,22 +1,21 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', function() {
+    'use strict';
 
-const nomber0fFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+    const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
 
-const personalMovieDB = {
-    count: nomber0fFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+    const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    };
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = prompt("На сколько оцените его?", ""),
-      c = prompt("Один из последних просмотренных фильмов?", ""),
-      d = prompt("На сколько оцените его?", "");
+    for (let i = numberOfFilms; i > 0; i--) {
+        let a = prompt("Один из последних просмотренных фильмов?", "");
+        let b = prompt("На сколько оцените его?", "");
+        personalMovieDB.movies[a] = b;
+    } 
 
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
+    console.log(personalMovieDB);
+});
